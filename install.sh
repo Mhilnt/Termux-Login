690c274a021888550d05141a9232d6949678f8f0
Mhilnt <83482235+Mhilnt@users.noreply.github.com>
2021-05-22 13:00:40 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg install python2
+clear
+pkg install python
+clear
+pip install requests
+clear
+pip2 install requests
+clear
+pip install mechanize
+clear
+pip2 install mechanize
+clear
+figlet -f small Success
